b730f684c295407ebee9cc0089dd90aed35b8e31
Eduardo Dicarte <eduardo_dicarte@yahoo.com.br>
2015-08-31 08:18:09 -0300

    Versionando Arquivos de Configuração

---
+++ servers/informadus/manifests/web.pp
@@ -0,0 +1,16 @@
+exec {"update Apache Server":
+	command => "/bin/yum update -y httpd"
+}
+
+package {"httpd":
+	ensure => installed,
+	require => Exec["update Apache Server"]
+}
+
+service {"httpd":
+	ensure => running,
+	enable => true,
+	hasstatus => true,
+	hasrestart => true,
+	require => Package["httpd"]
+}
