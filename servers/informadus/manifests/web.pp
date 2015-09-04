92c3a9b422a73a3fecff8110e0dc2df0a1b2fa91
Eduardo Dicarte <eduardo_dicarte@yahoo.com.br>
2015-09-04 19:43:55 -0300

    Configuração do servidor web e do serviço de ftp

--- servers/informadus/manifests/web.pp
+++ servers/informadus/manifests/web.pp
@@ -18,3 +18,25 @@ service {"httpd":
 package {"iptables":
 	ensure => "purged"
 }
+
+package {"vsftpd":
+	ensure => latest
+}
+
+service{"vsftpd":
+	ensure => running,
+	enable => true,
+	hasstatus => true,
+	hasrestart => true,
+	require => Package["vsftpd"]
+}
+	
+
+file {"/etc/hosts":
+	ensure => file
+
+}
+
+File['/etc/hosts']{
+	content => "192.168.33.30 informadusview.informadus"
+}
