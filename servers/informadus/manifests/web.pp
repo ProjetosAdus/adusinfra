3905fad81110e101e96aa217954e19a944b4e839
Eduardo Dicarte <eduardo_dicarte@yahoo.com.br>
2015-09-08 08:17:18 -0300

    Incluindo parametros para a configuração do FTP e adicionando ip a VM.

--- servers/informadus/manifests/web.pp
+++ servers/informadus/manifests/web.pp
@@ -1,10 +1,6 @@
-exec {"update Apache Server":
-	command => "/bin/yum update -y httpd"
-}
 
 package {"httpd":
-	ensure => installed,
-	require => Exec["update Apache Server"]
+	ensure => latest
 }
 
 service {"httpd":
@@ -19,24 +15,28 @@ package {"iptables":
 	ensure => "purged"
 }
 
-package {"vsftpd":
-	ensure => latest
+exec {"get repository":
+	command => "/usr/bin/rpm -iUvh http://ftp.astral.ro/mirrors/fedora/pub/epel/7/x86_64/e/epel-release-7-5.noarch.rpm"
 }
 
-service{"vsftpd":
+package {"proftpd":
+	ensure => installed,
+	require => Exec["get repository"]
+}
+
+service{"proftpd":
 	ensure => running,
 	enable => true,
 	hasstatus => true,
 	hasrestart => true,
-	require => Package["vsftpd"]
-}
-	
-
-file {"/etc/hosts":
-	ensure => file
-
+	require => File["/etc/sysconfig/proftpd"]
 }
 
-File['/etc/hosts']{
+file{"/etc/hosts":
 	content => "192.168.33.30 informadusview.informadus"
+}
+
+file{"/etc/sysconfig/proftpd":
+	content => "PROFTPD_OPTIONS='-DANONYMOUS_FTP'",
+	require => Package["proftpd"]
 }
