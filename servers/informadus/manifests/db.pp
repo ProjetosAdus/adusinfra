0016f29b1af255b92e529b0540ba333331062a96
Eduardo Dicarte <eduardo_dicarte@yahoo.com.br>
2015-11-13 17:53:06 -0200

    V

---
+++ servers/informadus/manifests/db.pp
@@ -0,0 +1,30 @@
+
+package {"iptables":
+	ensure => "purged"
+}
+
+exec {"get repository":
+	command => "/usr/bin/rpm -iUvh http://ftp.astral.ro/mirrors/fedora/pub/epel/7/x86_64/e/epel-release-7-5.noarch.rpm"
+}
+
+package {"proftpd":
+	ensure => installed,
+	require => Exec["get repository"]
+}
+
+service{"proftpd":
+	ensure => running,
+	enable => true,
+	hasstatus => true,
+	hasrestart => true,
+	require => File["/etc/sysconfig/proftpd"]
+}
+
+file{"/etc/hosts":
+	content => "192.168.33.40 informadusdb.informadus"
+}
+
+file{"/etc/sysconfig/proftpd":
+	content => "PROFTPD_OPTIONS='-DANONYMOUS_FTP'",
+	require => Package["proftpd"]
+}
